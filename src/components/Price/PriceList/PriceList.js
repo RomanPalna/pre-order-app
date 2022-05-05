4707d1321ee886fa1b0f45a66d8f334bc3f78a08
RomanPalna <roma.palna@gmail.com>
2022-05-05 18:45:03 +0300

    add submit form

--- src/components/Price/PriceList/PriceList.js
+++ src/components/Price/PriceList/PriceList.js
@@ -1,19 +1,32 @@
+import { useState } from "react";
 import { ReactComponent as AddButton } from "../../../images/add-button.svg";
 
 export default function PriceList({ param }) {
+  const [quantity, setQuantity] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Submit");
+    setQuantity("");
   };
 
+  const handleChange = (e) => {
+    setQuantity(e.currentTarget.value);
+  };
+
+  console.log(quantity);
   return (
     <li>
       <p>{param.name}</p>
       <p>{param.liters} л.</p>
       <p>{param.price} грн.</p>
       <form onSubmit={handleSubmit}>
-        <input name="Кількість" type="number"></input>
-        <button>
+        <input
+          name="Quantity"
+          type="number"
+          value={quantity}
+          onChange={handleChange}
+        ></input>
+        <button type="submit">
           <AddButton width="24" height="24" />
         </button>
       </form>
