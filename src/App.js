b04922ceea2d9ec04d2858f73a892c7cc39e7df4
RomanPalna <roma.palna@gmail.com>
2022-05-05 17:10:29 +0300

    add clock in main page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
 import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Clock from "./components/Main/Main";
 
 function App() {
   return (
     <>
       <Navigation />
       <Routes>
-        <Route path="/" element={<div>Заглушка Main</div>} />
+        <Route path="/" element={<Clock />} />
         <Route path="/price" element={<div>Заглушка Price</div>} />
         <Route path="/order" element={<div>Заглушка Order</div>} />
         <Route path="*" element={<PageNotFound />} />
